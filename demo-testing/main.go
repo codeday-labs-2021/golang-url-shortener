b9ec1cf8151c01814da78f888ec96d6dac966167
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-07-14 16:22:24 -0700

    update progress
    just wanted to show what i have been testing, i plan on making this a functional cli tool by the end of this week

--- demo-testing/main.go
+++ demo-testing/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math/rand"
+	"net/url"
 	"os"
 
-	// "strings"
-	"net/url"
-	"time"
+	// "time"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	gonanoid "github.com/matoous/go-nanoid/v2"
 )
 
@@ -32,52 +32,81 @@ func isUrl(str string) bool {
 in our case, the dictionary will probably be turned into a database or something.
 */
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	M := make(map[string]string)
-	idLength := 3
-	for {
-		fmt.Println("")
-		fmt.Println("Type (c/C) to check the stored data for this run")
-		fmt.Println("Type (s/S) to shorten/store a url")
-		fmt.Println("Type (l/L) to loop up a url using a key/id")
-		fmt.Print("Type anything else to quit:  ")
+	sc := bufio.NewScanner(os.Stdin)
+	fmt.Println("Type 'r' to run server or 'a' to add urls to the database:  ")
+	sc.Scan()
+	in := sc.Text()
 
-		sc := bufio.NewScanner(os.Stdin)
-		sc.Scan()
-		input := sc.Text()
-		fmt.Println("")
-		if input == "c" || input == "C" {
-			for id, url := range M {
-				fmt.Println("ID:", id, "=> URL:", url)
-			}
-		} else if input == "s" || input == "S" {
-			fmt.Println("Please paste your url: ")
+	if in == "r" || in == "R" {
+
+		r := gin.Default()
+
+		r.GET("/test", func(c *gin.Context) {
+			c.Request.URL.Path = "/test2"
+			r.HandleContext(c)
+		})
+		r.GET("/test2", func(c *gin.Context) {
+			c.JSON(200, gin.H{"hello": "world"})
+		})
+
+		r.GET("/test3", func(c *gin.Context) {
+			c.Redirect(http.StatusMovedPermanently, "http://www.google.com/")
+		})
+
+		r.Run(":8080")
+
+	} else {
+
+		M := make(map[string]string)
+		idLength := 3
+		for {
+			fmt.Println("")
+			fmt.Println("Type (c/C) to check the stored data for this run")
+			fmt.Println("Type (s/S) to shorten/store a url")
+			fmt.Println("Type (l/L) to loop up a url using a key/id")
+			fmt.Print("Type anything else to quit:  ")
+
 			sc.Scan()
-			inputURL := sc.Text()
-			if isUrl(inputURL) {
-				for {
-					currID := genID(idLength)
-					if _, ok := M[currID]; !ok {
-						M[currID] = inputURL
-						break
-					} else {
-						idLength += 1
+			input := sc.Text()
+			fmt.Println("")
+			if input == "c" || input == "C" {
+				for id, url := range M {
+					fmt.Println("ID:", id, "=> URL:", url)
+				}
+			} else if input == "s" || input == "S" {
+				fmt.Println("Please paste your url: ")
+				sc.Scan()
+				inputURL := sc.Text()
+				if isUrl(inputURL) {
+					for {
+						currID := genID(idLength)
+						if _, ok := M[currID]; !ok {
+							M[currID] = inputURL
+							// r.GET(currID, func(c *gin.Context) {
+							// 	c.Redirect(http.StatusMovedPermanently, inputURL)
+							// })
+
+							break
+						} else {
+							idLength += 1
+						}
 					}
+				} else {
+					fmt.Println("invalid url")
+				}
+			} else if input == "l" || input == "L" {
+				fmt.Println("Please paste an ID: ")
+				sc.Scan()
+				inputID := sc.Text()
+				if val, ok := M[inputID]; ok {
+					fmt.Println("Your Url is: " + val)
+				} else {
+					fmt.Println("The ID you provided is invalid")
 				}
 			} else {
-				fmt.Println("invalid url")
+				break
 			}
-		} else if input == "l" || input == "L" {
-			fmt.Println("Please paste an ID: ")
-			sc.Scan()
-			inputID := sc.Text()
-			if val, ok := M[inputID]; ok {
-				fmt.Println("Your Url is: " + val)
-			} else {
-				fmt.Println("The ID you provided is invalid")
-			}
-		} else {
-			break
 		}
 	}
+
 }
